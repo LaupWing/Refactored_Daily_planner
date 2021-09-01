043c7425a69b601cb446ffccc73c40f0e013cd2e
LaupWing <loc_strict@hotmail.com>
2021-09-01 13:02:55 +0200

    base login styling

---
+++ src/components/Globals/Field.vue
@@ -0,0 +1,26 @@
+<template>
+   <div class="flex flex-col my-2 w-full">
+      <label class="text-xs tracking-wide uppercase font-bold">{{label}}</label>
+      <input 
+         class="px-2 py-1 outline-none border-gray-300 rounded border"
+         type="text" 
+         v-model="value" 
+      />
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'Field',
+   props:{
+      value: {
+         type: String,
+         required: true
+      },
+      label: {
+         type: String,
+         required: true
+      },
+   }
+}
+</script>
--- src/views/Auth/index.vue
+++ src/views/Auth/index.vue
@@ -1,19 +1,23 @@
 <template>
    <div class="m-auto">
-      <form @submit.prevent="login">
+      <form class="w-72 flex flex-col" @submit.prevent="login">
          <h2 class="font-bold text-3xl">Sign in</h2>
-         <div class="field">
+         <!-- <div class="field">
             <label for="email">Email</label>
             <input type="email" name="email" v-model="email" />
-         </div>
-         <div class="field">
-            <label for="password">Password:</label>
-            <input type="password" name="password" v-model="password" />
+         </div> -->
+         <div class="flex flex-col my-4">
+            <app-field
+               value=""
+               label="Email"
+            />
+            <app-field
+               value=""
+               label="Password"
+            />
          </div>
          <p v-if="feedback">{{ feedback }}</p>
-         <div class="field">
-            <button>Submit</button>
-         </div>
+         <button class="bg-main-highlight font-bold uppercase text-sm tracking-wide p-2 rounded px-6">Submit</button>
       </form>
    </div>
 </template>
