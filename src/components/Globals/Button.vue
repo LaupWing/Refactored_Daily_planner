a741d54a8de0d26236c2f546df8b3f69409817b8
LaupWing <loc_strict@hotmail.com>
2021-09-01 13:07:15 +0200

    button component

---
+++ src/components/Globals/Button.vue
@@ -0,0 +1,13 @@
+<template>
+   <button 
+      class="bg-main-highlight-primary hover:bg-main-highlight-dark duration-200 font-bold uppercase text-sm tracking-wide p-2 rounded px-6"
+   >
+      <slot/>
+   </button>
+</template>
+
+<script>
+export default {
+   name: 'Button'
+}
+</script>
