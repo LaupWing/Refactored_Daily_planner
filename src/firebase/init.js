7f9922eb5e077d22bb566f176e9c3131ae4c590a
LaupWing <loc_strict@hotmail.com>
2021-09-01 12:29:36 +0200

    firebase initializaed

---
+++ src/firebase/init.js
@@ -0,0 +1,13 @@
+import {initializeApp} from 'firebase/app'
+
+var firebaseConfig = {
+   apiKey: "AIzaSyDTf5mLqKAd04TT_EtTM244aWkXkjgIQFU",
+   authDomain: "daily-planner-40cbf.firebaseapp.com",
+   databaseURL: "https://daily-planner-40cbf.firebaseio.com",
+   projectId: "daily-planner-40cbf",
+   storageBucket: "daily-planner-40cbf.appspot.com",
+   messagingSenderId: "307529607050",
+   appId: "1:307529607050:web:bb6a48978d7f0e11"
+}
+
+initializeApp(firebaseConfig)
--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -1,3 +1,9 @@
+// import { getAuth } from 'firebase'
+// import firebae from 'firebase'
+// const auth = getAuth()
+// console.log('auth --->' ,auth)
 export const actions = {
-   
+   async login() {
+
+   }
 }
