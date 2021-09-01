0391e4d473afcfbda6868635c350855c2e375bae
LaupWing <loc_strict@hotmail.com>
2021-09-01 13:48:52 +0200

    Added Form container

---
+++ src/views/Auth/FormContainer/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <form class="w-72 flex flex-col" @submit.prevent="login">
+         <slot/>
+   </form>
+</template>
+
+<script>
+export default {
+   name: 'FormContainer'
+}
+</script>
