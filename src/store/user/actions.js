2af9f4e706a10064b42d41a74e410d821739fc7b
LaupWing <loc_strict@hotmail.com>
2021-09-01 20:51:36 +0200

    signIn function

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,10 +1,18 @@
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
 const auth = getAuth()
 
 export const actions = {
    async login({commit}, {email, password}) {
       try{
          const user = await signInWithEmailAndPassword(auth, email, password)
+         commit('setUser', user)
+      }catch(e){
+         throw new Error(e)
+      }
+   },
+   async signIn({commit}, {email, password}){
+      try{
+         const user = await createUserWithEmailAndPassword(auth, email, password)
          commit('setUser', user)
       }catch(e){
          throw new Error(e)
