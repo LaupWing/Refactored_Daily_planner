4a938b5700a39c2a292ed76010b1aa07a4e89e9a
LaupWing <loc_strict@hotmail.com>
2021-09-02 21:11:05 +0200

    rendering stamp

--- src/views/Home/Day/Timeline/index.vue
+++ src/views/Home/Day/Timeline/index.vue
@@ -1,54 +1,12 @@
 <template>
    <ul>
-      <li data-time="00:00">00:00</li>
-      <li data-time="00:30">-</li>
-      <li data-time="01:00">01:00</li>
-      <li data-time="01:30">-</li>
-      <li data-time="02:00">02:00</li>
-      <li data-time="02:30">-</li>
-      <li data-time="03:00">03:00</li>
-      <li data-time="03:30">-</li>
-      <li data-time="04:00">04:00</li>
-      <li data-time="04:30">-</li>
-      <li data-time="05:00">05:00</li>
-      <li data-time="05:30">-</li>
-      <li data-time="06:00">06:00</li>
-      <li data-time="06:30">-</li>
-      <li data-time="07:00">07:00</li>
-      <li data-time="07:30">-</li>
-      <li data-time="08:00">08:00</li>
-      <li data-time="08:30">-</li>
-      <li data-time="09:00">09:00</li>
-      <li data-time="09:30">-</li>
-      <li data-time="10:00">10:00</li>
-      <li data-time="10:30">-</li>
-      <li data-time="11:00">11:00</li>
-      <li data-time="11:30">-</li>
-      <li data-time="12:00">12:00</li>
-      <li data-time="12:30">-</li>
-      <li data-time="13:00">13:00</li>
-      <li data-time="13:30">-</li>
-      <li data-time="14:00">14:00</li>
-      <li data-time="14:30">-</li>
-      <li data-time="15:00">15:00</li>
-      <li data-time="15:30">-</li>
-      <li data-time="16:00">16:00</li>
-      <li data-time="16:30">-</li>
-      <li data-time="17:00">17:00</li>
-      <li data-time="17:30">-</li>
-      <li data-time="18:00">18:00</li>
-      <li data-time="18:30">-</li>
-      <li data-time="19:00">19:00</li>
-      <li data-time="19:30">-</li>
-      <li data-time="20:00">20:00</li>
-      <li data-time="20:30">-</li>
-      <li data-time="21:00">21:00</li>
-      <li data-time="21:30">-</li>
-      <li data-time="22:00">22:00</li>
-      <li data-time="22:30">-</li>
-      <li data-time="23:00">23:00</li>
-      <li data-time="23:30">-</li>
-      <li data-time="00:00">00:00</li>
+      <li 
+         v-for="timestamp in timestamps"
+         :data-time="timestamp"
+         :key="timestamp"
+      >
+         {{timestamp.includes('30') ? '-' : timestamp}}
+      </li>
    </ul>
 </template>
 
