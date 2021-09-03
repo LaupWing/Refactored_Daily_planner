b99f41b968b6b4f82ed0c9b24e6e0d7c68516a2f
LaupWing <loc_strict@hotmail.com>
2021-09-03 13:00:47 +0200

    update fix

--- src/router/index.js
+++ src/router/index.js
@@ -32,7 +32,7 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-   if (to.name !== 'Auth' && store.state.user.user){
+   if (to.name !== 'Auth' && store.state.user){
       next()
    }else{
       if(to.name === 'Auth'){
