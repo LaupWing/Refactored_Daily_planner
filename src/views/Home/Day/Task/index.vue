31ac8979d8ee0ed076f41f7c75362c74330d189f
LaupWing <loc_strict@hotmail.com>
2021-09-05 16:38:09 +0200

    calculatePoint

--- src/views/Home/Day/Task/index.vue
+++ src/views/Home/Day/Task/index.vue
@@ -1,6 +1,9 @@
 <template>
    <div
-      class="absolute w-full flex flex-col duration-500 select-none"
+      class="absolute w-full flex flex-col duration-500 select-none h-36 bg-red-400"
+      :style="{
+         top: `${calculatePoint(task.time.begin)}px`
+      }"
    >
    </div>
 </template>
@@ -22,6 +25,24 @@ export default {
       top(){
 
       }
+   },
+   methods:{
+      calculatePoint(state) {
+         const li = this.timeline_positions.find((li) => {
+            const liHour = li.time.split(":")[0]
+            const taskHour = state.split(":")[0]
+            if (liHour === taskHour) {
+               return li
+            }
+         })
+         const calcMinutes = ((li.height * 2) / 60) * state.split(":")[1]
+         const point = li.midpoint + calcMinutes
+         return point
+      },
+   },
+   created(){
+      console.log(this.task)
+      console.log(this.calculatePoint(this.task.time.begin))
    }
 }
 </script>
