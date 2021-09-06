f9ac4e8e65f91225d632e3e45d70347995d3e7b0
LaupWing <loc_strict@hotmail.com>
2021-09-06 18:44:24 +0200

    cant be done

--- src/views/Home/Day/index.vue
+++ src/views/Home/Day/index.vue
@@ -1,61 +1,66 @@
 <template>
-   <div 
-      id="planner"
-      class="relative overflow-y-auto flex items-start"
-      :style="{
-         height: '50vh'
-      }"
-      @scroll="handleScroll"
-      ref="container"
-   >
+   <!-- <div class="flex flex-col relative"> -->
       <div 
-         @keyup.space.ctrl="test"
-         tabindex="0"
-         class="pointer-events-none fixed top-0 left-0 w-screen h-screen"
-      > 
-      </div>
-      <timeline
-         v-if="container_mounted"
-         :container="$refs.container"
-         :showed_task="showed_task"
-         :midpoint="midpoint"
-         ref="timeline"
-         @mounted="(e)=>{
-            createTimelinePositions(e)
-            setCurrentTime()
-         }"
-      />
-      <div 
+         id="planner"
+         class="relative overflow-y-auto flex items-start"
          :style="{
-            width
+            height: '50vh'
          }"
-         class="flex-1 mx-4 relative"
+         @scroll="handleScroll"
+         ref="container"
       >
-         <task
-            v-if="timeline_positions"
-            v-for="(task, index) in tasksOfToday"
-            :key="index"
-            :task="task"
-            :midpoint="midpoint"
-            :timeline_positions="timeline_positions"
-            @disable_showed_task="set_showed_task_null"
-            @set_showed_task="showed_task = $event"
-            @mounted="tasks_elements.push($event)"
-         />
-         <indicator
+         <div 
+            @keyup.space.ctrl="test"
+            tabindex="0"
+            class="pointer-events-none fixed top-0 left-0 w-screen h-screen"
+         > 
+         </div>
+         <timeline
             v-if="container_mounted"
-            :midpoint="midpoint"
-            :width="width"
             :container="$refs.container"
+            :showed_task="showed_task"
+            :midpoint="midpoint"
+            ref="timeline"
+            @mounted="(e)=>{
+               createTimelinePositions(e)
+               setCurrentTime()
+            }"
+         />
+         <div 
+            :style="{
+               width
+            }"
+            class="flex-1 mx-4 relative"
+         >
+            <task
+               v-if="timeline_positions"
+               v-for="(task, index) in tasksOfToday"
+               :key="index"
+               :task="task"
+               :midpoint="midpoint"
+               :timeline_positions="timeline_positions"
+               @disable_showed_task="set_showed_task_null"
+               @set_showed_task="showed_task = $event"
+               @mounted="tasks_elements.push($event)"
+            />
+            <indicator
+               v-if="container_mounted"
+               :midpoint="midpoint"
+               :width="width"
+               :container="$refs.container"
+            />
+         </div>
+         <controls
+            v-if="container_mounted"
+            :container="$refs.container"
+            :tasks_elements="tasks_elements"
+            :showed_task="showed_task"
          />
       </div>
-      <controls
-         v-if="container_mounted"
-         :container="$refs.container"
-         :tasks_elements="tasks_elements"
-         :showed_task="showed_task"
-      />
-   </div>
+      <!-- <div class="w-full absolute transform translate-y-full flex items-center justify-center bottom-0">
+         <icons-locked/>
+      </div> -->
+   <!-- </div> -->
 </template>
 
 <script>
