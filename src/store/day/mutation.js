3ae3ff2b82fd59defadcd636ce4027158dc18b47
LaupWing <loc_strict@hotmail.com>
2021-09-07 18:28:06 +0200

    day

---
+++ src/store/day/mutation.js
@@ -0,0 +1,5 @@
+export const mutations = {
+   setLocked(state, locked){
+      state.locked = locked
+   }
+}
---
+++ src/store/day/state.js
@@ -0,0 +1,4 @@
+export const state = {
+   colorLabels: null,
+   dailyTasks: null,
+}
