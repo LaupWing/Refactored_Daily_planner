6f2a965041ded655ac52277a056ac17d3e775112
LaupWing <loc_strict@hotmail.com>
2021-09-07 17:45:15 +0200

    current task

--- src/views/Home/Day/index.vue
+++ src/views/Home/Day/index.vue
@@ -1,24 +1,6 @@
 <template>
    <div class="flex flex-col">
-      <div 
-         class="w-full border overflow-hidden rounded-t text-main-font border-main-font flex ml-20"
-         @click="locked = !locked"
-         :style="{
-            width: '350px'
-         }"
-      >
-         <div class="flex-1 flex items-center text-sm text-center">
-            <div class="bg-main-font p-1 px-2 mr-2 text-white tracking-wider">
-               21:00
-            </div>
-            No current tasks
-         </div>
-         <div class="px-2 border-l border-main-font flex items-center justify-center cursor-pointer hover:bg-main-font hover:text-white">
-            <icon-plus 
-               class="w-5 group-hover:text-white"
-            />
-         </div>
-      </div>
+      <current-task/>
       <tasks-container
          ref="taskContainer"
          :locked="locked"
@@ -44,11 +26,13 @@
 
 <script>
 import TasksContainer from './TasksContainer'
+import CurrentTask from './CurrentTask'
 
 export default {
    name: 'Day',
    components:{
-      TasksContainer
+      TasksContainer,
+      CurrentTask
    },
    data(){
       return {
