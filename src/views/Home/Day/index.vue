ea9393467fa15ded35835895b23a567c93f1aa5c
LaupWing <loc_strict@hotmail.com>
2021-09-07 17:32:39 +0200

    update

--- src/views/Home/Day/index.vue
+++ src/views/Home/Day/index.vue
@@ -7,8 +7,8 @@
             width: '350px'
          }"
       >
-         <div class="flex-1 flex items-center text-center">
-            <div class="bg-main-font p-1 px-2 mr-1 text-white text-sm tracking-wider">
+         <div class="flex-1 flex items-center text-sm text-center">
+            <div class="bg-main-font p-1 px-2 mr-2 text-white tracking-wider">
                21:00
             </div>
             No current tasks
