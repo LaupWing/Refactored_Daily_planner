b005a8371345dd7738583e239e41a24d939f875b
LaupWing <loc_strict@hotmail.com>
2021-09-09 20:22:01 +0200

    fixed bug

--- src/store/_day/mutation.js
+++ src/store/_day/mutation.js
@@ -1,5 +1,10 @@
 export const mutations = {
    setProp(state, {type, value}){
+      if(state[type] === value){
+         return
+      }
+      console.log('setting', type)
+      console.log('setting', value)
       state[type] = value
    },
    set_showed_task_null(state, value){
