084ffe67910c6dd39ca1bfaaf7bc41c0793d8604
LaupWing <loc_strict@hotmail.com>
2021-09-09 16:42:56 +0200

    removed code

--- src/views/Home/Day/TasksContainer/index.vue
+++ src/views/Home/Day/TasksContainer/index.vue
@@ -117,23 +117,6 @@ export default {
          this.height = this.$el.offsetHeight
          this.midpoint = this.scrolled + (this.height/2)
       },
-      moveToCurrentTime(){
-         const {hours, minutes} = this.$store.getters['_day/time']
-
-         const distanceHours = this.$store.state._day.timeline_positions
-            .find(tl=>tl.time.split(':')[0]===String(hours))
-            .midpoint
-               
-         const li = this.$refs.timeline.$el.querySelector('li')
-         const totalDistance = li.offsetHeight * 2
-         const distancePerMinute = totalDistance/60
-         const distanceMinutes = distancePerMinute * minutes
-         const moveTo = ((distanceHours + distanceMinutes) - this.$refs.container.offsetHeight / 2)
-         
-         setTimeout(()=>{
-            this.$el.scrollTo(0, moveTo)
-         })
-      },
       converDateToMS(time){
          const date = new Date()
          const day = date.getDate()
