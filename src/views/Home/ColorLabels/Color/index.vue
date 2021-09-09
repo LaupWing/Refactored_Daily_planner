e3f40bb0709c4d0b85466f99c436667e5d0ba885
LaupWing <loc_strict@hotmail.com>
2021-09-09 17:36:20 +0200

    color splitted

---
+++ src/views/Home/ColorLabels/Color/index.vue
@@ -0,0 +1,23 @@
+<template>
+   <div
+      class="my-2 w-6 h-6 rounded-full border-main-font border-2 font-bold text-main-font text-sm"
+      :class="`
+         bg-main-task-${color} 
+         ${color === 'pink' ? 'w-40 px-2' : '' }
+      `"
+   >
+      {{color === 'pink' ? 'testing' : ''}}
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'Color',
+   props:{
+      color:{
+         type: String,
+         required: true
+      }
+   }
+}
+</script>
--- src/views/Home/ColorLabels/index.vue
+++ src/views/Home/ColorLabels/index.vue
@@ -1,27 +1,27 @@
 <template>
-   <div class="flex flex-col items-center absolute left-2 top-1/2 transform -translate-y-2/4">
+   <div class="flex flex-col items-start absolute left-2 top-1/2 transform -translate-y-2/4">
       <h2 class="text-sm tracking-wide uppercase font-bold text-main-font">Labels</h2>
-      <div 
+      <color 
          v-for="color in colors"
          :key="color"
-         :class="`my-2 w-6 h-6 rounded-full bg-main-task-${color} border-main-font border-2`"
-      >
-
-      </div>
+         :color="color"
+      />
    </div>
 </template>
 
 <script>
+import Color from './Color'
+
 export default {
    name: 'ColorLabels',
+   components:{
+      Color
+   },
    computed:{
       colors(){
          return new Set(this.$store.getters['planner/tasksOfToday']
             .map(x=>x.color.color))
       }
-   },
-   created(){
-      console.log(this.colors)
    }
 }
 </script>
