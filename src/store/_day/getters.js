6b7a4b7bb278e5ad2f2a47fa5379d11c636b3503
LaupWing <loc_strict@hotmail.com>
2021-09-09 16:06:38 +0200

    time

--- src/store/_day/getters.js
+++ src/store/_day/getters.js
@@ -1,3 +1,11 @@
 export const getters = {
-   
+   time(){
+      const date = new Date()
+      const hours = addZero(date.getHours())
+      const minutes = addZero(date.getMinutes())
+      return {
+         hours,
+         minutes
+      }   
+   }
 }
--- src/store/_day/state.js
+++ src/store/_day/state.js
@@ -1,5 +1,7 @@
 export const state = {
    locked: true,
    task_width: '350px',
-   showed_task: null
+   showed_task: null,
+   timeline_el: null,
+   container_el: null
 }
