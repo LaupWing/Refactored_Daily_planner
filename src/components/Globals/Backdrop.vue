fb3fb64d015c438d68ef021001b8a97f9f64890f
LaupWing <loc_strict@hotmail.com>
2021-09-10 16:29:53 +0200

    basic global backdorp

---
+++ src/components/Globals/Backdrop.vue
@@ -0,0 +1,14 @@
+<template>
+   <div 
+      class="w-screen h-screen fixed top-0 left-0"
+      @click="$emit('close')"
+   >
+      <slot @click.native.stop/>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'Backdrop'
+}
+</script>
