3a47981516112e6c85631fb058ac0b05b9cb067b
LaupWing <loc_strict@hotmail.com>
2021-09-10 19:56:23 +0200

    Color cmp

---
+++ src/views/Home/Day/TasksContainer/Task/ContextMenu/Color/index.vue
@@ -0,0 +1,18 @@
+<template>
+   <div 
+      class="bg-main-task-pink m-1.5 w-6 h-6 rounded-full"
+      :class="`bg-main-task-${color}`"
+   ></div>
+</template>
+
+<script>
+export default {
+   name: 'Color',
+   props:{
+      color:{
+         type: String,
+         required: true
+      }
+   }
+}
+</script>
