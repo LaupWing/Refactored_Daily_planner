034985e9886e0b9f3945d3c992a55e6a63b4e547
LaupWing <loc_strict@hotmail.com>
2021-09-10 18:27:37 +0200

    toggle show and not show

--- src/views/Home/Day/TasksContainer/Task/ContextMenu/index.vue
+++ src/views/Home/Day/TasksContainer/Task/ContextMenu/index.vue
@@ -1,9 +1,13 @@
 <template>
    <app-backdrop
-      @close="test"
+      v-on="$listeners"
    >
       <div 
-         class="w-28 h-12 bg-gray-600"
+         class="w-28 h-12 bg-gray-600 fixed"
+         :style="{
+            top: `${event.clientY}px`,
+            left: `${event.clientX}px`
+         }"
          @click.stop
       >
 
@@ -14,10 +18,19 @@
 <script>
 export default {
    name: 'ContextMenu',
+   props:{
+      event:{
+         required: true,
+         type: PointerEvent
+      }
+   },
    methods:{
       test(){
          console.log('Closing')
       }
+   },
+   created(){
+      console.log(this.event)
    }
 }
 </script>
