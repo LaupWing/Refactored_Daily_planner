6ae4ab9903523bc72b2ddce5ed0861fa7fabe928
LaupWing <loc_strict@hotmail.com>
2021-09-10 18:57:39 +0200

    addded trashcan

--- src/views/Home/Day/TasksContainer/Task/ContextMenu/index.vue
+++ src/views/Home/Day/TasksContainer/Task/ContextMenu/index.vue
@@ -3,15 +3,15 @@
       v-on="$listeners"
    >
       <div 
-         class="w-48 h-52 bg-white fixed rounded shadow flex flex-col"
+         class="w-48 bg-white fixed rounded shadow flex flex-col items-center"
          :style="{
             top: `${event.clientY}px`,
             left: `${event.clientX}px`
          }"
          @click.stop
       >
-         <h2 class="uppercase tracking-wider text-sm font-bold p-2 border-b border-main-font">{{task.task}}</h2>
-         <div class="flex flex-wrap p-2">
+         <h2 class="uppercase tracking-wider text-sm font-bold p-2 border-b border-main-font w-full">{{task.task}}</h2>
+         <div class="flex flex-wrap p-2 w-full border-b border-main-font">
             <div class="bg-main-task-pink m-1.5 w-6 h-6 rounded-full"></div>
             <div class="bg-main-task-purple m-1.5 w-6 h-6 rounded-full"></div>
             <div class="bg-main-task-blue m-1.5 w-6 h-6 rounded-full"></div>
@@ -20,6 +20,10 @@
             <div class="bg-main-task-orange m-1.5 w-6 h-6 rounded-full"></div>
             <div class="bg-main-task-green m-1.5 w-6 h-6 rounded-full"></div>
          </div>
+         <div class="flex w-full justify-center items-center py-2 border-b border-main-font hover:bg-red-300 hover:text-white cursor-pointer">
+            <icon-trashcan class="w-5"/>
+         </div>
+         <button class="px-3 py-0.5 my-2 uppercase rounded-full hover:bg-main-font hover:text-white border border-main-font text-xs tracking-wider">Confirm</button>
       </div>
    </app-backdrop>
 </template>
