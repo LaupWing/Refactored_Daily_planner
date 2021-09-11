32c190b289a6dac893c4da49b1e56463525e2fb1
LaupWing <loc_strict@hotmail.com>
2021-09-11 13:43:37 +0200

    added before

--- src/views/Home/Day/TasksContainer/Task/ContextMenu/Color/index.vue
+++ src/views/Home/Day/TasksContainer/Task/ContextMenu/Color/index.vue
@@ -1,7 +1,7 @@
 <template>
    <div 
-      class="m-1.5 w-6 h-6 rounded-full"
-      :class="`bg-main-task-${color}`"
+      class="m-1.5 w-6 h-6 rounded-full relative"
+      :class="`bg-main-task-${color} ${active ? 'active' : ''}`"
    ></div>
 </template>
 
@@ -12,7 +12,27 @@ export default {
       color:{
          type: String,
          required: true
+      },
+      active:{
+         type: String,
+         required: true
       }
+   },
+   created(){
+      console.log(this.color)
+      console.log(this.active)
    }
 }
 </script>
+
+<style scoped>
+.active::before{
+   content: '';
+   width: 140%;
+   height: 140%;
+   background: rgba(0,0,0,.2);
+   border-radius: 50%;
+   position: absolute;
+   z-index: -2;
+}
+</style>
